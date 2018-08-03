467320b5a11768f3be24393296fa60712b4308b2
Koos du Preez <35713435+kooscode@users.noreply.github.com>
2018-08-03 16:34:48 -0700

    Update pickerError.cpp

--- src/pickerError.cpp
+++ src/pickerError.cpp
@@ -1,11 +1,3 @@
-//
-//  pickerError.cpp
-//  color-picker
-//
-//  Created by Koos du Preez on 12/17/17.
-//  Copyright © 2017 TerraClear Inc. All rights reserved.
-//
-
 #include "pickerError.hpp"
 
 pickerError::pickerError(string errmsg)
